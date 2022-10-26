aeee93e875945e3b2e29349a1c484279fe47596c
RoseImai <68888945+RoseImai@users.noreply.github.com>
2022-10-26 20:31:48 +0600

    Added "heavy" function and single output

--- src/main/java/HtmlStorage.java
+++ src/main/java/HtmlStorage.java
@@ -1,5 +1,6 @@
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.math.BigInteger;
 import java.util.Random;
 
 public class HtmlStorage {
@@ -30,18 +31,22 @@ public class HtmlStorage {
     public static void params(PrintWriter output)
     {
         Random rand = new Random();
-        for(int i = 0; i<=100; i++)
+        double m=0;
+        int k;
+        for(int i = 0; i<=10000; i++)
         {
-            int k = rand.nextInt(1000);
-            output.println("HTTP/1.1 200 OK");
-            output.println("Content-Type: text/html; charset=utf-8");
-            output.println();
-            output.println("<html>");
-            output.println("<head><title>Function</title></head>");
-            output.println("<body><p>Some function generated " + k + "</p></body>");
-            output.println("</html>");
-            output.flush();
+            k = rand.nextInt(1000);
+            m+=k;
+            for (int j=0; j<=10000; j++) m *= 1.1;
         }
+        output.println("HTTP/1.1 200 OK");
+        output.println("Content-Type: text/html; charset=utf-8");
+        output.println();
+        output.println("<html>");
+        output.println("<head><title>Function</title></head>");
+        output.println("<body><p>Some function generated " + m + "</p></body>");
+        output.println("</html>");
+        output.flush();
     }
 
     public static void defaultPage(PrintWriter output)
